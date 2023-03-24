b65493b3c91ceb5117c60a77738e4f86a27f7317
SebSense <sebastian.senic@gmail.com>
2023-03-24 11:16:42 +0100

    CLEANUP LOAD, LIST, NEW
    Refactored commands into method calls:
    - LoadDictionary(string path, string file)
    - ShowDictionary()
    - AddWord(string word_swe, string word_eng)

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@
         }   
         static void Main(string[] args)
         {
-            //FIXME: defaultPath is customized for navigating VS project directories. Change before publishing.
+            //FIXME: path is customized for navigating VS project directories. Change before publishing.
             string defaultPath = "..\\..\\..\\dict\\";
             string defaultFile = "sweeng.lis";
             Console.WriteLine("Welcome to the dictionary app!");
@@ -44,62 +44,21 @@
                 }
                 else if (command == "load")
                 {
-                    //TBD: Refactor repeating code
-                    //TBD: Add console feedback for file not found.
-                    if (argument.Length == 2)
-                    {
-                        //NYI: Crashes on incorrect filename FileNotFoundException
-                        //NYI: Check file formatting before emptying list and trying to load.
-                        using (StreamReader reader = new StreamReader(defaultPath + argument[1]))
-                        {
-                            dictionary = new List<SweEngGloss>(); // Empty it!
-                            string line = reader.ReadLine();
-                            while (line != null)
-                            {
-                                SweEngGloss gloss = new SweEngGloss(line);
-                                dictionary.Add(gloss);
-                                line = reader.ReadLine();
-                            }
-                        }
-                        Console.WriteLine("\n " + argument[1] + " succesfully loaded!\n");
-                    }
+                    if (argument.Length <= 2)
+                        LoadDictionary(defaultPath, argument[1]);
                     else if (argument.Length == 1)
-                    {
-                        using (StreamReader reader = new StreamReader(defaultPath + defaultFile))
-                        {
-                            dictionary = new List<SweEngGloss>(); // Empty it!
-                            string line = reader.ReadLine();
-                            while (line != null)
-                            {
-                                SweEngGloss gloss = new SweEngGloss(line);
-                                dictionary.Add(gloss);
-                                line = reader.ReadLine();
-                            }
-                        }
-                        Console.WriteLine("\n " + defaultFile + " succesfully loaded!\n");
-                    }
+                        LoadDictionary(defaultPath, defaultFile);
                 }
                 else if (command == "list")
                 {
-                    if (!dictionary.Any()) Console.WriteLine(" There are no words in the dictionary!");
-                    else
-                        foreach (SweEngGloss gloss in dictionary)
-                           Console.WriteLine($"{gloss.word_swe,-10}  - {gloss.word_eng,-10}");
+                    ShowDictionary();
                 }
                 else if (command == "new")
                 {
                     if (argument.Length == 3)
-                    {
-                        dictionary.Add(new SweEngGloss(argument[1], argument[2]));
-                        Console.WriteLine(" Added word to dictionary: '{0} - {1}'\n", argument[1], argument[2]);
-                    }
+                        AddWord(argument[1], argument[2]);
                     else if (argument.Length == 1)
-                    {
-                        string swe_meaning = GetString("Write word in Swedish: ");
-                        string eng_meaning = GetString("Write word in English: ");
-                        dictionary.Add(new SweEngGloss(swe_meaning, eng_meaning));
-                        Console.WriteLine(" Added word to dictionary: '{0} - {1}'\n", swe_meaning, eng_meaning);
-                    }
+                        AddWord(GetString("Write word in Swedish: "), GetString("Write word in English: "));
                 }
                 else if (command == "delete")
                 {
@@ -198,6 +157,38 @@
                 }
             }
             while (true);
+        }
+
+        private static void AddWord(string word_swe, string word_eng)
+        {
+            dictionary.Add(new SweEngGloss(word_swe, word_eng));
+            Console.WriteLine(" Added word to dictionary: '{0} - {1}'\n", word_swe, word_eng);
+        }
+
+        private static void ShowDictionary()
+        {
+            if (!dictionary.Any()) Console.WriteLine(" There are no words in the dictionary!");
+            else
+                foreach (SweEngGloss gloss in dictionary)
+                    Console.WriteLine($"{gloss.word_swe,-10}  - {gloss.word_eng,-10}");
+        }
+
+        private static void LoadDictionary(string path, string file)
+        {
+            //NYI: Crashes on incorrect filename FileNotFoundException. Add user feedback instead.
+            //NYI: Check file formatting before emptying list and trying to load.
+            using (StreamReader reader = new StreamReader(path + file))
+            {
+                dictionary = new List<SweEngGloss>(); // Empty it!
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    SweEngGloss gloss = new SweEngGloss(line);
+                    dictionary.Add(gloss);
+                    line = reader.ReadLine();
+                }
+            }
+            Console.WriteLine("\n " + file[1] + " succesfully loaded!\n");
         }
 
         private static string[] GetArgs(string prompt)
